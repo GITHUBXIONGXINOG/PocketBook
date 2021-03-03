47bc8a800407cf22fa2b6bd0265514e1a79382ae
GITHUBXIONGXINOG <2017211677@stu.cqupt.edu.cn>
2021-03-03 22:18:27 +0800

    first commit

---
+++ src/components/initPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="initPanel">
+    <h2 class="title">请选择CSV文件</h2>
+    <input type="file">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'initPanel',
+  props: {
+    // msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ .initPanel {
+   
+ } 
+</style>
